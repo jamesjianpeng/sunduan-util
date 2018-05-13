3a053f83540ebe2bfff630c477af959979624b22
彭涧 <2393375953@qq.com>
2018-05-13 19:25:36 +0800

    feat: Increase the array and helper folders and change the export method
    1.Added pop and forEach methods. 2. Added buildURL and type object with data type judgment. 3. The
    export of hello and throttle is changed to module.exports
    
    BREAKING CHANGE: Start new array method

---
+++ test/array/foEach.js
@@ -0,0 +1,13 @@
+var forEach = require('../../lib/array/forEach');
+
+var obj = { a: 10, b: 20, c: 30 };
+forEach(obj, function(value, index, obj) {
+  console.log(value, index, obj);
+})
+
+console.log('============');
+
+var list = [1, 2, 3, 4];
+forEach(list, function(value, index, obj) {
+  console.log(value, index, obj);
+})
