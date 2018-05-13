858059d47264d36108c8b6c8214379a25543f977
彭涧 <2393375953@qq.com>
2018-05-13 19:34:33 +0800

    fix: Modify the export method of all methods

---
+++ index.js
@@ -0,0 +1,13 @@
+const hello = require('./lib/hello');
+const throttle = require('./lib/throttle');
+const pop = require('./lib/array/pop');
+const forEach = require('./lib/array/forEach');
+const buildURL = require('./lib/helper/buildURL');
+// let ajax = require('./lib/throttle').ajax;
+
+exports.hello = hello
+exports.throttle = throttle
+exports.pop = pop
+exports.forEach = forEach
+exports.buildURL = buildURL
+// exports.ajax = ajax
